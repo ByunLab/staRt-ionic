6dc71199347b0a5546487c0b94686af8af24f472
Jon Chin <jon@sharebyteaching.org>
2019-08-28 16:00:50 -0400

    added user labels to always show current clinician and participant

--- staRt/www/common-components/practice-directive/practice-directive_controller.js
+++ staRt/www/common-components/practice-directive/practice-directive_controller.js
@@ -96,6 +96,11 @@ var practiceDirective = angular.module( 'practiceDirective' );
 practiceDirective.controller( 'PracticeDirectiveController',
 			      function($scope, $timeout, $localForage, AutoService, NotifyingService, FirebaseService, ProfileService, SessionStatsService, StartUIState, UploadService, $rootScope, $state, $http, $cordovaDialogs, ToolbarService)
     {
+	ProfileService.getCurrentProfile().then((profile) => {
+	    $scope.participant_name = profile.name;
+	    $scope.clinician_name = FirebaseService.userName();;
+	});
+
 	// var uploadURLs = [
 	// 	"http://localhost:5000",
 	// 	"http://localhost:5000",
