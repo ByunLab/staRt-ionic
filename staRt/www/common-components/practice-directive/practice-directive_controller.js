ef6658031efd23091dbede6be26a339bb2900c1a
Helen Carey <hmc385@nyu.edu>
2019-11-25 19:17:58 -0500

    fixes merge conflicts

--- staRt/www/common-components/practice-directive/practice-directive_controller.js
+++ staRt/www/common-components/practice-directive/practice-directive_controller.js
@@ -615,7 +615,6 @@ practiceDirective.controller( 'PracticeDirectiveController', function($scope, $t
 			function (res) {
 				if (res) {
 					beginPracticeForUser(res);
-					if ($scope.startPracticeCallback) $scope.startPracticeCallback();
 				} else {
 					if (navigator.notification) {
 						navigator.notification.alert('Can\'t start ' + sessionDisplayString() + ' -- create a profile first', null, 'No profile');
@@ -701,7 +700,6 @@ practiceDirective.controller( 'PracticeDirectiveController', function($scope, $t
 		} else if (window.AudioPlugin !== undefined) {
 			AudioPlugin.stopRecording(recordingDidStop, recordingDidFail);
 		}
-		if ($scope.endPracticeCallback) $scope.endPracticeCallback();
 	};
 
 	$scope.nextWord = function() {
