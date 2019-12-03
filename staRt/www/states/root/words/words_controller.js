c3906b6c402a4aa7fd286d79183a30e506e8ddcb
Will Haack <willhaacktutoring@gmail.com>
2019-12-03 16:06:44 -0600

    fixed eslint errors and semicolon problems in the words controller

--- staRt/www/states/root/words/words_controller.js
+++ staRt/www/states/root/words/words_controller.js
@@ -9,7 +9,7 @@
 
 	words.controller('WordsController', function($scope, $timeout, $localForage, AutoService, FirebaseService, NotifyingService, ProfileService, StartUIState, $rootScope, $state, $cordovaDialogs)
 	{
-    console.log('WordsController here!');
+		console.log('WordsController here!');
 
 		var lastChronoTime;
 		var ticker = undefined;
@@ -18,30 +18,30 @@
 			if (ticker) {
 				var nextChronoTime = Date.now();
 				var duration = nextChronoTime - lastChronoTime;
-				NotifyingService.notify("quest-tick", duration);
+				NotifyingService.notify('quest-tick', duration);
 				lastChronoTime = nextChronoTime;
 			}
-		}
+		};
 
-		var clearTimeLogger = function() {;
+		var clearTimeLogger = function() {
 			if (ticker) {
 				logInterval();
 				clearInterval(ticker);
 				ticker = undefined;
 			}
-		}
+		};
 
 		$scope.practicing = false;
 		$scope.configuring = false;
-		$scope.order = "random";
+		$scope.order = 'random';
 		$scope.data = {
-      categoriesRestricted: !!$rootScope.categoryRestrictions,
-      categoryString: null,
+			categoriesRestricted: !!$rootScope.categoryRestrictions,
+			categoryString: null,
 			count: $rootScope.rootTrialCount || 50,
 			csvs: [],
-			navTitle: "Quest",
-      selectedCategories: [],
-      trialCountSelectorDisabled: !!$rootScope.rootTrialCount,
+			navTitle: 'Quest',
+			selectedCategories: [],
+			trialCountSelectorDisabled: !!$rootScope.rootTrialCount,
 			type: null,
 			waveHidden: false
 		};
@@ -51,74 +51,74 @@
 
 
 		$scope.categoryNames = [
-			"Syllabic /r/",
-			"Consonantal Front",
-			"Consonantal Back",
-			"Vocalic Front",
-			"Vocalic Back"
+			'Syllabic /r/',
+			'Consonantal Front',
+			'Consonantal Back',
+			'Vocalic Front',
+			'Vocalic Back'
 		];
 
 		$scope.syllableCategoryCSVs = [
-			"data/sp_vocalic_all.csv",
-			"data/sp_consonantal_front.csv",
-			"data/sp_consonantal_back.csv",
-			"data/sp_vocalic_front.csv",
-			"data/sp_vocalic_back.csv"
+			'data/sp_vocalic_all.csv',
+			'data/sp_consonantal_front.csv',
+			'data/sp_consonantal_back.csv',
+			'data/sp_vocalic_front.csv',
+			'data/sp_vocalic_back.csv'
 		];
 
 		$scope.wordCategoryCSVs = [
-			"data/wp_vocalic_all.csv",
-			"data/wp_consonantal_front.csv",
-			"data/wp_consonantal_back.csv",
-			"data/wp_vocalic_front.csv",
-			"data/wp_vocalic_back.csv"
+			'data/wp_vocalic_all.csv',
+			'data/wp_consonantal_front.csv',
+			'data/wp_consonantal_back.csv',
+			'data/wp_vocalic_front.csv',
+			'data/wp_vocalic_back.csv'
 		];
 
 		$scope.goBackToLanding = function() {
 			console.log('back button clicked');
 			$scope.practicing = false;
 			$scope.configuring = false;
-		}
+		};
 
 		$scope.beginSyllableQuestConfiguration = function() {
-			console.log("Begin syllable quest configuration");
-			$scope.data.navTitle = "Syllable Quest";
+			console.log('Begin syllable quest configuration');
+			$scope.data.navTitle = 'Syllable Quest';
 			$scope.practicing = false;
 			$scope.data.categoryString = null;
 			$scope.data.selectedCategories = [];
 			$scope.data.csvs = [];
 			$scope.configuring = true;
-			$scope.order = "random";
-      $scope.data.type = "Syllable";
-      if ($rootScope.categoryRestrictions) {
-        for (var i = 0; i < $rootScope.categoryRestrictions.length; i++) {
-          $scope.toggleRCategory($rootScope.categoryRestrictions[i], true);
-        }
-      }
-		}
+			$scope.order = 'random';
+			$scope.data.type = 'Syllable';
+			if ($rootScope.categoryRestrictions) {
+				for (var i = 0; i < $rootScope.categoryRestrictions.length; i++) {
+					$scope.toggleRCategory($rootScope.categoryRestrictions[i], true);
+				}
+			}
+		};
 
 		$scope.beginWordQuestConfiguration = function() {
-			console.log("Begin word quest configuration");
-			$scope.data.navTitle = "Word Quest";
+			console.log('Begin word quest configuration');
+			$scope.data.navTitle = 'Word Quest';
 			$scope.practicing = false;
 			$scope.data.categoryString = null;
 			$scope.data.selectedCategories = [];
 			$scope.data.csvs = [];
 			$scope.configuring = true;
-			$scope.order = "random";
-      $scope.data.type = "Word";
-      if ($rootScope.categoryRestrictions) {
-        for (var i = 0; i < $rootScope.categoryRestrictions.length; i++) {
-          $scope.toggleRCategory($rootScope.categoryRestrictions[i], true);
-        }
-      }
-		}
+			$scope.order = 'random';
+			$scope.data.type = 'Word';
+			if ($rootScope.categoryRestrictions) {
+				for (var i = 0; i < $rootScope.categoryRestrictions.length; i++) {
+					$scope.toggleRCategory($rootScope.categoryRestrictions[i], true);
+				}
+			}
+		};
 
 		$scope.beginQuest = function() {
-			console.log("Begin " + $scope.data.type + " quest");
+			console.log('Begin ' + $scope.data.type + ' quest');
 			$scope.practicing = true;
 			$scope.configuring = false;
-			$scope.order = "random";
+			$scope.order = 'random';
 			$rootScope.finalSelectedCategories = $scope.data.selectedCategories;
 
 
@@ -128,7 +128,7 @@
 
 
 			// Start a timer to log the time spend in quest mode play
-			NotifyingService.notify("quest-start");
+			NotifyingService.notify('quest-start');
 			lastChronoTime = Date.now();
 			ticker = setInterval(logInterval, 60000);
 		};
@@ -137,40 +137,40 @@
 			$scope.practicing = false;
 			$scope.configuring = false;
 			$scope.data.csvs = [];
-			$scope.data.navTitle = "Quest";
+			$scope.data.navTitle = 'Quest';
 			clearTimeLogger();
 		};
 
 		$scope.toggleRCategory = function(wordCategoryIdx, force) {
-      if (!force && !!$rootScope.categoryRestrictions) {
-        $cordovaDialogs.alert("When resuming a session, you cannot change /r/ categories", "Session in progress");
-        return;
-      }
+			if (!force && !!$rootScope.categoryRestrictions) {
+				$cordovaDialogs.alert('When resuming a session, you cannot change /r/ categories', 'Session in progress');
+				return;
+			}
 
 			var idx = $scope.data.selectedCategories.indexOf(wordCategoryIdx);
 			if (idx === -1) {
-				$scope.data.selectedCategories.push(wordCategoryIdx)
+				$scope.data.selectedCategories.push(wordCategoryIdx);
 				$scope.data.selectedCategories.sort();
-        AutoService.toggleCategoryRestriction(wordCategoryIdx, 1);
+				AutoService.toggleCategoryRestriction(wordCategoryIdx, 1);
 			} else {
-        $scope.data.selectedCategories.splice(idx, 1);
-        AutoService.toggleCategoryRestriction(wordCategoryIdx, 0);
+				$scope.data.selectedCategories.splice(idx, 1);
+				AutoService.toggleCategoryRestriction(wordCategoryIdx, 0);
 			}
 
-			var csvSource = $scope.data.type === "Word" ? $scope.wordCategoryCSVs : $scope.syllableCategoryCSVs;
+			var csvSource = $scope.data.type === 'Word' ? $scope.wordCategoryCSVs : $scope.syllableCategoryCSVs;
 
 			$scope.data.csvs = $scope.data.selectedCategories.map( function (x) { return csvSource[x]; } );
 			var categories = $scope.data.selectedCategories.map( function (x) { return $scope.categoryNames[x]; } );
-			$scope.data.categoryString = categories.join(", ");
-		}
+			$scope.data.categoryString = categories.join(', ');
+		};
 
 		$scope.updateCount = function() {
-			console.log("count is now " + $scope.data.count);
-		}
+			console.log('count is now ' + $scope.data.count);
+		};
 
 		$scope.$on('$destroy', function() {
 			clearTimeLogger();
-    });
+		});
 
 		if ($rootScope.sessionToResume){
 			$rootScope.categoryRestrictions = $rootScope.sessionToResume.categoryRestrictions;
