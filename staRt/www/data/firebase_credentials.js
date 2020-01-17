0205ef582de27fdae389c54a5b235c63bdbfd48c
Helen Carey <hmc385@nyu.edu>
2020-01-17 13:16:33 -0500

    See #432. Annotates and improves sandbank logic for 'highlight an approaching milestone' feature. Should be ready for merge.

---
+++ staRt/www/data/firebase_credentials.js
@@ -0,0 +1,8 @@
+var firebaseCredentials = {
+	'apiKey': 'AIzaSyAhXwmRqVnZUd1sKaMxNjTCAMm7-iZZFHQ',
+	'authDomain': 'start-app-59c52.firebaseapp.com',
+	'databaseURL': 'https://start-app-59c52.firebaseio.com',
+	'projectId': 'start-app-59c52',
+	'storageBucket': 'start-app-59c52.appspot.com',
+	'messagingSenderId': '242774896654',
+};
