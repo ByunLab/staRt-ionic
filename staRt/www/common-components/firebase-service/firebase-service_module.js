84396de379000ab81ad980813b8825a55a9ddbb7
Will Haack <willhaacktutoring@gmail.com>
2020-01-22 22:56:54 -0500

    we now are more sneaky

--- staRt/www/common-components/firebase-service/firebase-service_module.js
+++ staRt/www/common-components/firebase-service/firebase-service_module.js
@@ -23,14 +23,8 @@ firebaseService.factory('FirebaseService', function()
 		signInFlow: 'popup'
 	};
 
-	var firebaseApp = firebase.initializeApp({
-		apiKey: 'AIzaSyAhXwmRqVnZUd1sKaMxNjTCAMm7-iZZFHQ',
-		authDomain: 'start-app-59c52.firebaseapp.com',
-		databaseURL: 'https://start-app-59c52.firebaseio.com',
-		projectId: 'start-app-59c52',
-		storageBucket: 'start-app-59c52.appspot.com',
-		messagingSenderId: '242774896654'
-	});
+
+	var firebaseApp = firebase.initializeApp(firebaseCredentials);
 
 	var db = firebaseApp.firestore();
 	var settings = {timestampsInSnapshots: true};
