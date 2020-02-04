dbdd08eded4527615213749ef41ebfcbab0fb9fa
-h <helencarey@users.noreply.github.com>
2020-02-03 19:40:03 -0500

    Deleted personal file

--- staRt/www/data/firebase_credentials.js
+++
@@ -1,8 +0,0 @@
-var firebaseCredentials = {
-	'apiKey': 'AIzaSyAhXwmRqVnZUd1sKaMxNjTCAMm7-iZZFHQ',
-	'authDomain': 'start-app-59c52.firebaseapp.com',
-	'databaseURL': 'https://start-app-59c52.firebaseio.com',
-	'projectId': 'start-app-59c52',
-	'storageBucket': 'start-app-59c52.appspot.com',
-	'messagingSenderId': '242774896654',
-};
